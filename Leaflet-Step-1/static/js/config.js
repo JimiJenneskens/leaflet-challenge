f556c7312a73a9ccac5dadbfe4d7a430e3b6c123
Jimi <jimijenneskens@gmail.com>
2020-11-27 17:10:46 -0600

    1finishedalmost

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
-// API key for mapbox
-const API_KEY = "pk.eyJ1Ijoia2lwbWFkZGVuIiwiYSI6ImNrOGdqcGx4eDAyZDYzbm12cHgwMW8xMTIifQ.wbuiW-LxCReXzCfC7O3ODQ";
+// API key
+const API_KEY = "pk.eyJ1IjoiamltaWplbm5lc2tlbnMiLCJhIjoiY2toZTE3NDBoMGNjbjJ4bGV0N3U4cTc5bSJ9.REvxrWjqFrp-MpY5lyoKqw";
